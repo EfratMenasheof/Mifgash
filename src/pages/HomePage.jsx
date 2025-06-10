9969aa860bb34c9626c6cb5116d0e968d4a770fa
Efrat Menasheof <efratmenasheof@Efrats-Mac.local>
2025-06-10 17:45:42 +0300

    google login

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,49 @@
-import { useState } from 'react';
 import MifgashCard from '../components/MifgashCard';
 import FriendsSection from '../components/FriendsSection';
 import { mockFriends } from '../data/FriendsData';
 import ProfileModal from '../components/ProfileModal';
 import FriendsMap from '../components/FriendsMap';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function HomePage() {
+  const [user, setUser] = useState(null);
   const [selectedFriend, setSelectedFriend] = useState(null);
   const mifgashFriend = mockFriends.find(f => f.name === 'Daniel Radcliffe');
 
+  useEffect(() => {
+    axios.get('http://localhost:4000/api/current-user', { withCredentials: true })
+      .then(res => {
+        setUser(res.data); // יהיה null אם לא מחובר
+      })
+      .catch(err => {
+        console.error('Error fetching user:', err);
+      });
+  }, []);
+
+  if (!user) {
+    return (
+      <div className="text-center mt-5">
+        <h2>Please log in</h2>
+        <a href="http://localhost:4000/auth/google">
+          <button className="btn btn-primary">Login with Google</button>
+        </a>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
-      {/* משפט Welcome */}
-      <h1 className="welcome-title mt-4 mb-1">Welcome back, Maya!</h1>
+      <h1 className="welcome-title mt-4 mb-1">Welcome back, {user.name}!</h1>
       <h5 className="homepage-subtitle">
-  Bringing people closer, one word at a time 🫱🏻‍🫲🏼
-</h5>
+        Bringing people closer, one word at a time 🫱🏻‍🫲🏼
+      </h5>
 
       <div className="row align-items-stretch gx-5 gx-md-7">
-        {/* שמאל – חברים */}
         <div className="col-md-6 ps-md-4 d-flex flex-column justify-content-start">
           <FriendsSection />
         </div>
 
-        {/* ימין – מפגש מעל מפה */}
         <div className="col-md-6 pe-md-4 d-flex flex-column justify-content-start">
           <div className="mb-3">
             <MifgashCard
