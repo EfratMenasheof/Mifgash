5684796490418511aa68f8ee0d952ec3648568c5
Efrat Menasheof <efratmenasheof@Efrats-Mac.local>
2025-06-15 19:27:20 +0300

    vvv

--- src/App.jsx
+++ src/App.jsx
@@ -44,9 +44,14 @@ function App() {
     return (
       <div className="container text-center mt-5">
         <h2>Welcome to Mifgash!</h2>
-        <a href={`${import.meta.env.VITE_BACKEND_URL}/auth/google`}>
-          <button className="btn btn-primary mt-3">Login with Google</button>
-        </a>
+        <button
+          className="btn btn-primary mt-3"
+          onClick={() =>
+            window.location.href = `${import.meta.env.VITE_BACKEND_URL}/auth/google`
+          }
+        >
+          Login with Google
+        </button>
       </div>
     );
   }
